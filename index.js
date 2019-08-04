056b1b9f2a80e2f6e086f6116c2293a831d7b3de
daksha-aeer <50169911+daksha-aeer@users.noreply.github.com>
2019-08-04 22:23:50 +0530

    sign up created

--- index.js
+++ index.js
@@ -4,19 +4,22 @@ firebase.auth().onAuthStateChanged(function(user) {
       
       document.getElementById("user_div").style.display ="block";
       document.getElementById("login_div").style.display = "none";
+      document.getElementById("signup_div").style.display = "none";
 
       var user = firebase.auth().currentUser;
+      document.getElementById("user_para").innerHTML = user.email;
 
       if (user != null) {
         var email_id = user.email;
         
-        document.getElementById("user_para").innerHTML = "Welcome User : "+ email;
+        
 
       }
     } else {
       // No user is signed in.
       document.getElementById("user_div").style.display = "none";
       document.getElementById("login_div").style.display = "block";
+      document.getElementById("signup_div").style.display = "none";
     }
   });
 
@@ -38,5 +41,27 @@ function logout () {
 
   firebase.auth().signOut();
   alert ("logged out")
+
+}
+
+function signup () {
+
+  var userEmail = document.getElementById("email_field1").value;
+
+  var userPass = document.getElementById("password_field1").value;
+  alert ("welcome"+ userEmail);
+  firebase.auth().createUserWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
+  });
+ 
+}
+
+function open_signup () {
+
+  document.getElementById("signup_div").style.display ="block";
+  document.getElementById("login_div").style.display = "none";        
 
 }
